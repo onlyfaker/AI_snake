0a44c07cdf886b3eb05d9bc66cb266c37e2b4ca8
Gemaljevic Aleksandar <67740316+onlyfaker@users.noreply.github.com>
2025-06-13 20:45:51 -0400

    Update model.py

--- model.py
+++ model.py
@@ -31,5 +31,20 @@ class QTrainer:
         self.criterion = nn.MSELoss()
 
     def train_step(self,state,action,reward,next_state, game_over):
-        pass
+        state = torch.tensor(state,dtype=torch.float)
+        next_state = torch.tensor(next_state, dtype=torch.float)
+        action = torch.tensor(action,dtype=torch.long)
+        reward = torch.tensor(reward,dtype=torch.float)
+        #(n,x)
+
+        if len(state.shape) == 1:
+            #(1,x)
+            state = torch.unsqueeze(state,0)
+            next_state = torch.unsqueeze(next_state, 0)
+            action = torch.unsqueeze(action, 0)
+            reward = torch.unsqueeze(reward, 0)
+            game_over = (game_over,)#touple with 1 value
+            
+
+
 
