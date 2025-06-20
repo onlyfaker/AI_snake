0334bf246723cf1ee0e96e900ea0a2f3641f2f20
Gemaljevic Aleksandar <67740316+onlyfaker@users.noreply.github.com>
2025-06-20 18:23:20 -0400

    update

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ Point = namedtuple('Point', 'x, y')
 # rgb colors
 WHITE = (255, 255, 255)
 RED = (200, 0, 0)
-BLUE1 = (0, 0, 255)
-BLUE2 = (0, 100, 255)
+GREEN = (0, 255, 27)
+YELLOW = (255, 231, 0)
 BLACK = (0, 0, 0)
 
 BLOCK_SIZE = 20
@@ -114,8 +114,8 @@ class SnakeGame:
         self.display.fill(BLACK)
 
         for pt in self.snake:
-            pygame.draw.rect(self.display, BLUE1, pygame.Rect(pt.x, pt.y, BLOCK_SIZE, BLOCK_SIZE))
-            pygame.draw.rect(self.display, BLUE2, pygame.Rect(pt.x + 4, pt.y + 4, 12, 12))
+            pygame.draw.rect(self.display, GREEN, pygame.Rect(pt.x, pt.y, BLOCK_SIZE, BLOCK_SIZE))
+            pygame.draw.rect(self.display, YELLOW, pygame.Rect(pt.x + 4, pt.y + 4, 12, 12))
 
         pygame.draw.rect(self.display, RED, pygame.Rect(self.food.x, self.food.y, BLOCK_SIZE, BLOCK_SIZE))
 
